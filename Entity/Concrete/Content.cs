b644fdd5f58384d66a16ebfd8a85ba73a658351f
= <=>
2021-04-10 23:00:16 +0300

    Entity Katmanı Classlar Oluşturlulmaya Başlandı.

---
+++ Entity/Concrete/Content.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class Content
+    {
+        public int ContentId { get; set; }
+        public string ContentName { get; set; }
+        public DateTime ContentDate { get; set; }
+       
+    }
+}
