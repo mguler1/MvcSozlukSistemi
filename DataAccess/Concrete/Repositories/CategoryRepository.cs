f1abe89754563b793bbafd10891e27b550ceef0c
= <=>
2021-04-25 18:52:54 +0300

    Katman düzenlemeleri

---
+++ DataAccess/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        public void Delete(Category p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Category p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
