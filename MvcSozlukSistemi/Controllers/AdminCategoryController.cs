72c4e9cbb1364c62716d2de0f38570ab829c7762
= <=>
2021-05-11 02:27:23 +0300

    Tema eklendi

---
+++ MvcSozlukSistemi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,47 @@
+﻿using Business.ValidationRules;
+using DataAccess.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSozlukSistemi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var category = cm.GetList();
+            return View(category);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category x )
+        {
+            CategoryValidator categoryvalidator = new CategoryValidator();
+            ValidationResult result = categoryvalidator.Validate(x);
+            if (result.IsValid)
+            {
+                cm.CategoryAdd(x);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
