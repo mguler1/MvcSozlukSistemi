d2a478a5a385be6735070aa37fa3a8fc66873367
= <=>
2021-05-19 22:43:54 +0300

    Yazar Listeleme ekranı yapıldı

---
+++ MvcSozlukSistemi/Controllers/WriterüController.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSozlukSistemi.Controllers
+{
+    public class WriterüController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var writervalues = wm.GetList();
+            return View(writervalues);
+        }
+    }
+}
