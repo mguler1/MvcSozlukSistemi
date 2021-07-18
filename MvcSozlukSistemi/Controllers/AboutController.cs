f5e7fca1082cfc87275f2d2b149a6d76742a5ee0
= <=>
2021-07-18 12:10:02 +0300

    About işlemleri tamamlandı

--- MvcSozlukSistemi/Controllers/AboutController.cs
+++ MvcSozlukSistemi/Controllers/AboutController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,26 @@ namespace MvcSozlukSistemi.Controllers
     public class AboutController : Controller
     {
         // GET: About
+        AboutManager amanager = new AboutManager(new EfAboutDal());
         public ActionResult Index()
         {
+            var aboutvalue = amanager.GetList();
+            return View(aboutvalue);
+        }
+        [HttpGet]
+        public ActionResult AboutAdd()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AboutAdd(About x)
+        {
+            amanager.AboutAdd(x);
+            return RedirectToAction("Index");
+        }
+        public PartialViewResult AboutPartial()
+        {
+            return PartialView();
         }
     }
 }
