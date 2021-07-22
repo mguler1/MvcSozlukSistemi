b26640f76beaa72fc8dd7d54aba248c2fde85521
= <=>
2021-07-22 09:17:53 +0300

    Galeri işlemleri tamamlandı

---
+++ MvcSozlukSistemi/Controllers/GalleryController.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSozlukSistemi.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        ImageFileManager im = new ImageFileManager(new EfImageFileDal());
+        public ActionResult Index()
+        {
+            var image = im.GetList();
+            return View(image);
+        }
+    }
+}
