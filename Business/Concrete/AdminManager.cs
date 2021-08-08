f5857fb1ba57ef58fccf4f2bd204ea2d50ce3658
= <=>
2021-08-08 13:07:12 +0300

    Admin işlemleri tamamlandı

---
+++ Business/Concrete/AdminManager.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        IAdminDal _adminDal;
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+        public void AdminDelete(Admin admin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AdminUpdate(Admin admin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AdminAdd(Admin admin)
+        {
+            _adminDal.Insert(admin);
+        }
+
+        public Admin GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Admin> GetList()
+        {
+           return  _adminDal.List();
+        }
+    }
+}
---
+++ MvcSozlukSistemi/Controllers/AuthorizationController.cs
@@ -0,0 +1,34 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSozlukSistemi.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        // GET: Authorization
+       AdminManager lm = new AdminManager(new EfAdminDal());
+        public ActionResult Index()
+        {
+            var admin = lm.GetList();
+            return View(lm);
+        }
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAdmin(Admin a)
+        {
+            lm.AdminAdd(a);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
